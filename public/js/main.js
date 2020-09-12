dddb4be840f8beafd7e0b0fcd5772395388e5def
webnetkz <8gg001@gmail.com>
2020-09-13 01:44:50 +0600

    appented new moduls, ajax/form and request func

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,1 @@
-import {addUserData} from './moduls/userData.js';
-import {getGeo} from './moduls/user/geo.js';
-import { getBrowser } from './moduls/user/browser.js';
 
-// Добавление данных пользователя в localStorage
-addUserData(); // userAgent, language
-getGeo(); // Geo position
-getBrowser(); // Get browser
